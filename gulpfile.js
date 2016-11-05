80b8d704989a2f3661feeec2c7987f77ae06f6f9
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-11-05 10:18:10 +0100

    Trigger test & coverage from Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 var sonarqubeScanner = require('sonarqube-scanner');
 
-gulp.task('default', function (callback) {
+gulp.task('default', ['test'], function (callback) {
     // We just run a SonarQube analysis and push it to SonarQube.com
     // ----------------------------------------------------
     sonarqubeScanner({
@@ -9,11 +11,27 @@ gulp.task('default', function (callback) {
         token: process.env.SONARQUBE_TOKEN,
         options: {
             "sonar.projectName" : "SonarQube package for Atlasboard",
+            "sonar.sources" : "jobs, widgets",
             "sonar.tests" : "jobs, widgets",
             "sonar.test.inclusions" : "**/test/**",
-            "sonar.javascript.jstest.reportsPath" : "reports",
-            "sonar.javascript.lcov.reportPath" : "reports/lcov.info"
+            "sonar.javascript.jstest.reportsPath" : "coverage",
+            "sonar.javascript.lcov.reportPath" : "coverage/lcov.info"
         }
     }, callback);
     // ----------------------------------------------------
+});
+
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(['widgets/**/test/*.js', 'jobs/**/test/*.js'])
+    .pipe(mocha())
+    // Creating the reports after tests ran
+    .pipe(istanbul.writeReports());
+});
+
+gulp.task('pre-test', function () {
+  return gulp.src(['widgets/**/*.js', 'jobs/**/*.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
 });
